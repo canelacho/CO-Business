92b641b1e8a51ac5b9de6cee39c9fb8a5c47bf30
Ivan Torrellas <ivantorrellas@gmail.com>
2015-09-16 16:33:53 -0430

    Wall fixed

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -4,7 +4,7 @@ angular.module('coAppApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('main', {
-        url: '/wall',
+        url: '/login',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
 
